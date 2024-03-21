f79995ddd08681150ce0eb2e27322ee00955beda
Will Harrison <willharrison2016@gmail.com>
2024-03-20 21:12:06 -0500

    Create errorhandler.py

---
+++ utils/errorhandler.py
@@ -0,0 +1,9 @@
+class ErrorHandler:
+    """
+    A helper class for handling errors in a Kivy application.
+    """
+
+    @staticmethod
+    def handle_exception(exc, user_friendly_message=""):
+        # Log the exception, inform the user, etc.
+        pass
