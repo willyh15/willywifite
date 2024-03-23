2ed23c712d707d064de689c4c0f6e73d70387139
Will Harrison <willharrison2016@gmail.com>
2024-03-22 21:29:53 -0500

    Update scan.py

--- screens/scan.py
+++ screens/scan.py
@@ -1,19 +1,20 @@
-# screens/scan_networks_screen.py
-
 from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.adapters.listadapter import ListAdapter
 from kivy.uix.listview import ListView
 
-# Assuming these are your custom modules
 from controllers.network_controller import NetworkController
 from services.network_scan_service import NetworkScanService
+from utils.errorhandler import ErrorHandler
+from utils.background import BackgroundProcess
 
 class ScanNetworksScreen(Screen):
     def __init__(self, **kwargs):
         super(ScanNetworksScreen, self).__init__(**kwargs)
         self.network_controller = NetworkController(NetworkScanService())
+        self.error_handler = ErrorHandler()
+        self.background_process = BackgroundProcess()
 
         layout = BoxLayout(orientation='vertical')
         
@@ -25,8 +26,8 @@ class ScanNetworksScreen(Screen):
             data=self.networks_data,
             cls=Button,
             args_converter=lambda row_index, rec: {'text': rec['text'],
-                                                    'size_hint_y': None,
-                                                    'height': 40}
+                                                   'size_hint_y': None,
+                                                   'height': 40}
         )
         self.networks_list = ListView(adapter=self.list_adapter)
         
@@ -35,7 +36,7 @@ class ScanNetworksScreen(Screen):
         refresh_btn = Button(text='Refresh', size_hint_x=None, width=100)
         back_btn = Button(text='Back', size_hint_x=None, width=100)
 
-        refresh_btn.bind(on_press=self.refresh_networks)
+        refresh_btn.bind(on_press=self.on_refresh_press)
         back_btn.bind(on_press=lambda x: self.manager.current = 'homepage')
         
         button_layout.add_widget(refresh_btn)
@@ -46,9 +47,23 @@ class ScanNetworksScreen(Screen):
         
         self.add_widget(layout)
 
-    def refresh_networks(self, instance):
-        # Call perform_scan from NetworkController
+    def on_refresh_press(self, instance):
+        # Handle the refresh action with error handling and a background process
+        self.error_handler.handle(self.background_process.run, self.perform_scan, on_error=self.on_scan_error)
+
+    def perform_scan(self):
+        # Call perform_scan from NetworkController in the background
         new_data = self.network_controller.perform_scan()
+        # Schedule the update on the main thread since it modifies the UI
+        self.dispatch('on_update', new_data)
+
+    def on_scan_error(self, error):
+        # Handle scan errors (e.g., show an error message to the user)
+        pass
+
+    def on_update(self, *args):
+        # Triggered when new scan data is ready to be displayed
+        new_data = args[1]
         self.update_network_list(new_data)
 
     def update_network_list(self, new_data):
@@ -56,5 +71,4 @@ class ScanNetworksScreen(Screen):
         self.list_adapter.data = new_data
         self.networks_list._trigger_reset_populate()
 
-# Note: Ensure that the ListView adapter and the update method are properly configured for your version of Kivy.
-
+ScanNetworksScreen.register_event_type('on_update')
