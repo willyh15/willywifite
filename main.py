7723383ca5567ac4615116a68527741dabb6366d
Will Harrison <willharrison2016@gmail.com>
2024-03-22 21:19:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
+# main.py
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
-from screens.home import HomeScreen
-from screens.config import ConfigScreen
-from utils.configmanager import ConfigurationManager
-from utils.eventbinder import EventBinder
-# Other imports...
+from screens.home_screen import HomeScreen
+from screens.scan_networks_screen import ScanNetworksScreen
+from controllers.home_controller import HomeController
+from controllers.network_controller import NetworkController
 
-class MainApp(App):
+class MyApp(App):
     def build(self):
-        self.config_manager = ConfigurationManager()
-        self.event_binder = EventBinder()
-        self.screen_manager = ScreenManager()
+        # Controllers
+        home_controller = HomeController()
+        network_controller = NetworkController()
 
-        # Initialize your screens here
-        home_screen = HomeScreen(name='homepage')
-        config_screen = ConfigScreen(name='configuration')
+        # Screen Manager
+        screen_manager = ScreenManager()
 
-        # Bind events using EventBinder
-        self.event_binder.bind(home_screen)
-        self.event_binder.bind(config_screen)
+        # Screens
+        home_screen = HomeScreen(name='home', controller=home_controller)
+        scan_screen = ScanNetworksScreen(name='scan', controller=network_controller)
 
-        # Add screens to the ScreenManager
-        self.screen_manager.add_widget(home_screen)
-        self.screen_manager.add_widget(config_screen)
+        # Adding screens to ScreenManager
+        screen_manager.add_widget(home_screen)
+        screen_manager.add_widget(scan_screen)
 
-        # Continue with other screens and utils setup...
-        
-        return self.screen_manager
+        return screen_manager
 
 if __name__ == '__main__':
-    MainApp().run()
+    MyApp().run()
